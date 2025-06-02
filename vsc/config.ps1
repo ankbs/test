30b15ed2406d5d7c4cc019ca0e911c3d3d2a8cb6
Michael Kirst-Neshva [MVP Office 365] <mkn@ankbs.de>
2025-06-02 10:18:51 +0200

    module

---
+++ vsc/config.ps1
@@ -0,0 +1,7 @@
+# Zentrale Standardwerte und Konstanten
+
+$script:DefaultLogFolder = "C:\Temp\script\"
+$script:DefaultMSPPartner = "Cloud Security && Compliance Services"
+$script:DefaultMSPNameAP  = "Michael Kirst-Neshva"
+$script:DefaultMSPMail    = "support@domainen.io"
+$script:DefaultMSPURL     = "https://www.domainen.io"
