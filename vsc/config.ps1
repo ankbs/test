d9006f81ffd4987e290512b298396a4ad7e49e0f
Michael Kirst-Neshva [MVP Office 365] <mkn@ankbs.de>
2025-06-04 10:29:01 +0200

    Delete vsc directory

--- vsc/config.ps1
+++
@@ -1,7 +0,0 @@
-# Zentrale Standardwerte und Konstanten
-
-$script:DefaultLogFolder = "C:\Temp\script\"
-$script:DefaultMSPPartner = "Cloud Security && Compliance Services"
-$script:DefaultMSPNameAP  = "Michael Kirst-Neshva"
-$script:DefaultMSPMail    = "support@domainen.io"
-$script:DefaultMSPURL     = "https://www.domainen.io"
